2d4c57bbb482f0ad20038266f7bb82293f74f854
Shepherd <10481417+shepherd-06@users.noreply.github.com>
2023-04-20 10:44:33 +0300

    code and css updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from "./logo.svg";
 import "./css/App.css";
+import HeartRecordView from "./component/heart";
 
 function App() {
   return (
-    <div className="container">
-      <h1> Hello World!</h1>
+    <div className="primary_background">
+      <div className="container">
+        <h1 className="h1" align="center" style={{color: "whitesmoke", paddingTop: "10px"}}> Smart Shenanigans: Our IoT Project</h1>
+      
+        {/* old record view */}
+        <HeartRecordView></HeartRecordView>
+      
+      </div>
     </div>
   );
 }
