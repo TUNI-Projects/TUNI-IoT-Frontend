8df852418eff9e804bed64b5e5cf87bd582cfea0
Ibtehaz <10481417+shepherd-06@users.noreply.github.com>
2023-04-20 13:22:00 +0300

    bg :/

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <div className="container">
         <h1 className="h1" align="center" style={{ color: "whitesmoke", paddingTop: "10px" }}> Smart Shenanigans</h1>
         <h3 className="h4" align="center" style={{ color: "whitesmoke", }}> Our IoT Project</h3>
-        <hr />
+        <hr style={{ backgroundColor: "white", height: "3px" }} />
         {/* old record view */}
         {/* <HeartRecordView></HeartRecordView> */}
 
