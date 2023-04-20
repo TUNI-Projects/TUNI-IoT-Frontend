342853221c469b39cc8340f9881825325479e75a
Ibtehaz <10481417+shepherd-06@users.noreply.github.com>
2023-04-20 13:18:18 +0300

    changed bg

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ function App() {
   return (
     <div className="primary_background">
       <div className="container">
-        <h1 className="h1" align="center" style={{color: "whitesmoke", paddingTop: "10px"}}> Smart Shenanigans: Our IoT Project</h1>
-      
+        <h1 className="h1" align="center" style={{ color: "whitesmoke", paddingTop: "10px" }}> Smart Shenanigans</h1>
+        <h3 className="h4" align="center" style={{ color: "whitesmoke", }}> Our IoT Project</h3>
+        <hr />
         {/* old record view */}
-        <HeartRecordView></HeartRecordView>
-      
+        {/* <HeartRecordView></HeartRecordView> */}
+
       </div>
     </div>
   );
